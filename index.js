3e328f64180d31c5819fe1360a5c7c0d95f1bc16
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-16 16:49:16 +0100

    add comment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ playButton.addEventListener('click',
         console.log (myContainer);
 
         for (let i = 0; i < 100; i++){
-
+                                        // Aggiungo i parametri
             let nuovoElemento = createBox("div", "box", [i + 1]);
 
             // Aggiungo il click to change 
@@ -16,7 +16,7 @@ playButton.addEventListener('click',
                     nuovoElemento.classList.toggle('clicked');
                 }
             );
-
+            // Aggiungo nuovoElemento a myContainer
             myContainer.appendChild(nuovoElemento);
         }
     }
@@ -30,6 +30,7 @@ function createBox(tipoElemento, classe, insertData){
     // Creo un elemento div
     const mioElemnt = document.createElement (tipoElemento);
     mioElemnt.classList.add(classe);
+    // Aggiungo un parametro alla funzione per aggiungere gli elementi
     const addData = document.createTextNode(insertData);
     mioElemnt.appendChild(addData);
 
